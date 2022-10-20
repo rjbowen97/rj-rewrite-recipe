6787ad4fbfb353412a0900c3274322a392e108e4
rjbowen <rjbowen97@gmail.com>
2022-10-20 17:03:39 -0400

    Checking for methods within a class marked as private or final.

--- src/main/java/com/rj/StaticMethodRecipe.java
+++ src/main/java/com/rj/StaticMethodRecipe.java
@@ -4,14 +4,6 @@ import org.openrewrite.ExecutionContext;
 import org.openrewrite.Recipe;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
-import org.openrewrite.marker.Markers;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static java.util.Collections.emptyList;
-import static org.openrewrite.Tree.randomId;
-import static org.openrewrite.java.tree.Space.EMPTY;
 
 public class StaticMethodRecipe extends Recipe {
 
@@ -32,38 +24,29 @@ public class StaticMethodRecipe extends Recipe {
 
     private static class StaticMethodVisitor extends JavaIsoVisitor<ExecutionContext> {
 
-        private final J.Modifier staticModifier = new J.Modifier(randomId(),
-                                                                 EMPTY.withWhitespace(" "),
-                                                                 Markers.EMPTY,
-                                                                 J.Modifier.Type.Static,
-                                                                 emptyList());
-
-
-
         @Override
-        public J.VariableDeclarations.NamedVariable visitVariable(J.VariableDeclarations.NamedVariable variable, ExecutionContext executionContext) {
-            return super.visitVariable(variable, executionContext);
-        }
+        public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
+            J.ClassDeclaration modifiedClassDecl = super.visitClassDeclaration(classDecl, executionContext);
 
-        @Override
-        public J.VariableDeclarations visitVariableDeclarations(J.VariableDeclarations multiVariable, ExecutionContext executionContext) {
-            return super.visitVariableDeclarations(multiVariable, executionContext);
-        }
+            boolean classHasNonOverridableMethods = classDecl.getBody()
+                                                             .getStatements()
+                                                             .stream()
+                                                             .filter(statement -> statement instanceof J.MethodDeclaration)
+                                                             .map(J.MethodDeclaration.class::cast)
+                                                             .anyMatch(methodDeclaration -> methodDeclaration.getModifiers()
+                                                                                                             .stream()
+                                                                                                             .map(J.Modifier::getType)
+                                                                                                             .anyMatch(
+                                                                                                                     type -> type.equals(
+                                                                                                                             J.Modifier.Type.Private) || type.equals(
+                                                                                                                             J.Modifier.Type.Final)));
 
-        @Override
-        public J.MethodDeclaration visitMethodDeclaration(J.MethodDeclaration method, ExecutionContext c) {
-            if (method.hasModifier(J.Modifier.Type.Static)) {
-                return method;
+            if (classHasNonOverridableMethods) {
+                // TODO - Insert method checks here (checking for methods with references only to static class variables)
+                return modifiedClassDecl;
             }
 
-            if (method.hasModifier(J.Modifier.Type.Private) || method.hasModifier(J.Modifier.Type.Final)) {
-                List<J.Modifier> modifiers = new ArrayList<>(method.getModifiers());
-                modifiers.add(staticModifier);
-
-                method = method.withModifiers(modifiers);
-            }
-
-            return method;
+            return classDecl;
         }
     }
 }

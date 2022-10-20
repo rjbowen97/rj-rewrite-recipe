f5cd0d7d1a0b662a4de5bd611b79ed8e5c93e952
rjbowen <rjbowen97@gmail.com>
2022-10-20 17:18:56 -0400

    Refactoring to make the code easier to read.

--- src/main/java/com/rj/StaticMethodRecipe.java
+++ src/main/java/com/rj/StaticMethodRecipe.java
@@ -5,6 +5,9 @@ import org.openrewrite.Recipe;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.J;
 
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class StaticMethodRecipe extends Recipe {
 
     @Override
@@ -28,18 +31,21 @@ public class StaticMethodRecipe extends Recipe {
         public J.ClassDeclaration visitClassDeclaration(J.ClassDeclaration classDecl, ExecutionContext executionContext) {
             J.ClassDeclaration modifiedClassDecl = super.visitClassDeclaration(classDecl, executionContext);
 
-            boolean classHasNonOverridableMethods = classDecl.getBody()
-                                                             .getStatements()
-                                                             .stream()
-                                                             .filter(statement -> statement instanceof J.MethodDeclaration)
-                                                             .map(J.MethodDeclaration.class::cast)
-                                                             .anyMatch(methodDeclaration -> methodDeclaration.getModifiers()
-                                                                                                             .stream()
-                                                                                                             .map(J.Modifier::getType)
-                                                                                                             .anyMatch(
-                                                                                                                     type -> type.equals(
-                                                                                                                             J.Modifier.Type.Private) || type.equals(
-                                                                                                                             J.Modifier.Type.Final)));
+            Stream<J.MethodDeclaration> methodDeclarations = classDecl.getBody()
+                                                                      .getStatements()
+                                                                      .stream()
+                                                                      .filter(statement -> statement instanceof J.MethodDeclaration)
+                                                                      .map(J.MethodDeclaration.class::cast);
+
+            Predicate<J.MethodDeclaration> methodDeclarationIsNonOverrideable = methodDeclaration -> methodDeclaration.getModifiers()
+                                                                                                                      .stream()
+                                                                                                                      .map(J.Modifier::getType)
+                                                                                                                      .anyMatch(
+                                                                                                                              type -> type.equals(
+                                                                                                                                      J.Modifier.Type.Private) || type.equals(
+                                                                                                                                      J.Modifier.Type.Final));
+
+            boolean classHasNonOverridableMethods = methodDeclarations.anyMatch(methodDeclarationIsNonOverrideable);
 
             if (classHasNonOverridableMethods) {
                 // TODO - Insert method checks here (checking for methods with references only to static class variables)

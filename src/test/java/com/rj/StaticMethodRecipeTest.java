4e6838fcb004b5063e7cf5aa1165433cd053b4ab
rjbowen <rjbowen97@gmail.com>
2022-10-21 17:12:05 -0400

    Updated test case to more thoroughly test recipe

--- src/test/java/com/rj/StaticMethodRecipeTest.java
+++ src/test/java/com/rj/StaticMethodRecipeTest.java
@@ -20,45 +20,119 @@ class StaticMethodRecipeTest implements RewriteTest {
             //There is an overloaded version or rewriteRun that allows the RecipeSpec to be customized specifically
             //for a given test. In this case, the parser for this test is configured to not log compilation warnings.
             java("""
-                package com.org;
                 public class Utilities {
                     private static String magicWord = "magic";
-                    private boolean thisIsTrue = true;
-
-
+                    private boolean magicWordIsLocked = true;
+                    private static final String helloWorldString = "Hello world!";
+                
+                    // STATIC
                     private String getMagicWord() {
                         return magicWord;
                     }
-
+                
+                    // NOT STATIC
                     private void setMagicWord(String newMagicWord) {
-                        if (thisIsTrue) {
+                        if (!magicWordIsLocked) {
+                            magicWord = newMagicWord;
                         }
-                        magicWord = newMagicWord;
                     }
-                    
+                
+                    // NOT STATIC
+                    public boolean getMagicWordIsLocked() {
+                        return magicWordIsLocked;
+                    }
+                
+                    // NOT STATIC
+                    public void setMagicWordIsLocked(boolean magicWordIsLocked) {
+                        this.magicWordIsLocked = magicWordIsLocked;
+                    }
+                
+                    // NOT STATIC
                     public String helloWorld() {
-                        return "Hello world!";
+                        return helloWorldString;
+                    }
+                
+                    private static class Toolbox {
+                        private String wrench = "wrench";
+                        private static String drill = "drill";
+                
+                        // NOT STATIC
+                        public String getWrench() {
+                            return wrench;
+                        }
+                
+                        // NOT STATIC
+                        public void setWrench(String wrench) {
+                            this.wrench = wrench;
+                        }
+                
+                        // STATIC
+                        private String getDrill() {
+                            return drill;
+                        }
+                
+                        // NOT STATIC
+                        public void setDrill(String drill) {
+                            Toolbox.drill = drill;
+                        }
                     }
                 }
                 """, """
-                package com.org;
                 public class Utilities {
                     private static String magicWord = "magic";
-                    private boolean thisIsTrue = true;
-
-                    
+                    private boolean magicWordIsLocked = true;
+                    private static final String helloWorldString = "Hello world!";
+                
+                    // STATIC
                     private static String getMagicWord() {
                         return magicWord;
                     }
-                    
+                
+                    // NOT STATIC
                     private void setMagicWord(String newMagicWord) {
-                        if (thisIsTrue) {
+                        if (!magicWordIsLocked) {
+                            magicWord = newMagicWord;
                         }
-                        magicWord = newMagicWord;
                     }
-                    
+                
+                    // NOT STATIC
+                    public boolean getMagicWordIsLocked() {
+                        return magicWordIsLocked;
+                    }
+                
+                    // NOT STATIC
+                    public void setMagicWordIsLocked(boolean magicWordIsLocked) {
+                        this.magicWordIsLocked = magicWordIsLocked;
+                    }
+                
+                    // NOT STATIC
                     public String helloWorld() {
-                        return "Hello world!";
+                        return helloWorldString;
+                    }
+                
+                    private static class Toolbox {
+                        private String wrench = "wrench";
+                        private static String drill = "drill";
+                
+                        // NOT STATIC
+                        public String getWrench() {
+                            return wrench;
+                        }
+                
+                        // NOT STATIC
+                        public void setWrench(String wrench) {
+                            this.wrench = wrench;
+                        }
+                
+                        // STATIC
+                        private static String getDrill() {
+                            return drill;
+                        }
+                
+                        // NOT STATIC
+                        public void setDrill(String drill) {
+                            Toolbox.drill = drill;
+                        }
                     }
                 }
                 """));

760ad3bce0a392a34993e5f4eac4d1a3f8d3dc3c
rjbowen <rjbowen97@gmail.com>
2022-10-23 19:09:46 -0400

    Added testing for anonymous class declarations within methods.

--- src/test/java/org/openrewrite/java/StaticMethodRecipeTest.java
+++ src/test/java/org/openrewrite/java/StaticMethodRecipeTest.java
@@ -19,167 +19,216 @@ class StaticMethodRecipeTest implements RewriteTest {
             //There is an overloaded version or rewriteRun that allows the RecipeSpec to be customized specifically
             //for a given test. In this case, the parser for this test is configured to not log compilation warnings.
             java("""
-                import java.io.IOException;
-                import java.io.ObjectStreamException;
-                import java.io.Serializable;
-                
-                public class Utilities implements Serializable {
-                    private static String magicWord = "magic";
-                    private boolean magicWordIsLocked = true;
-                    private static final String helloWorldString = "Hello world!";
+                    import java.io.IOException;
+                    import java.io.ObjectStreamException;
+                    import java.io.Serializable;
                     
-                    public boolean a,b = true;
-                
-                    // STATIC
-                    private String getMagicWord() {
-                        return magicWord;
-                    }
-                
-                    // NOT STATIC
-                    private void setMagicWord(String newMagicWord) {
-                        if (!magicWordIsLocked) {
-                            magicWord = newMagicWord;
-                        }
-                    }
-                
-                    // NOT STATIC
-                    public boolean getMagicWordIsLocked() {
-                        return magicWordIsLocked;
-                    }
-                
-                    // NOT STATIC
-                    public void setMagicWordIsLocked(boolean magicWordIsLocked) {
-                        this.magicWordIsLocked = magicWordIsLocked;
-                    }
-                
-                    // NOT STATIC
-                    public String helloWorld() {
-                        return helloWorldString;
-                    }
-                
-                    // NOT STATIC
-                    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-                        out.write(magicWord.getBytes());
-                    }
-                
-                    // NOT STATIC
-                    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-                        in.defaultReadObject();
-                        magicWord = (String) in.readObject();
-                    }
-                
-                    // NOT STATIC
-                    private void readObjectNoData() throws ObjectStreamException {
-                        magicWord = "Just make something up!";
-                    }
-                
-                    private static class Toolbox {
-                        private String wrench = "wrench";
-                        private static String drill = "drill";
-                
-                        // NOT STATIC
-                        public String getWrench() {
-                            return wrench;
-                        }
-                
-                        // NOT STATIC
-                        public void setWrench(String wrench) {
-                            this.wrench = wrench;
-                        }
-                
+                    public class Utilities implements Serializable {
+                        private static String magicWord = "magic";
+                        private boolean magicWordIsLocked = true;
+                        private static final String helloWorldString = "Hello world!";
+                    
+                        public boolean a, b = true;
+                        public static boolean c, d = true;
+                    
                         // STATIC
-                        private String getDrill() {
-                            return drill;
+                        private String getMagicWord() {
+                            return magicWord;
                         }
-                
+                    
                         // NOT STATIC
-                        public void setDrill(String drill) {
-                            Toolbox.drill = drill;
+                        private String getExtraMagicalWord() {
+                            if (a && b) {
+                                return "Extra " + magicWord;
+                            }
+                            return magicWord;
+                        }
+                    
+                        // NOT STATIC
+                        private void setMagicWord(String newMagicWord) {
+                            if (!magicWordIsLocked) {
+                                magicWord = newMagicWord;
+                            }
+                        }
+                    
+                        // NOT STATIC
+                        public boolean getMagicWordIsLocked() {
+                            return magicWordIsLocked;
+                        }
+                    
+                        // NOT STATIC
+                        public void setMagicWordIsLocked(boolean magicWordIsLocked) {
+                            this.magicWordIsLocked = magicWordIsLocked;
+                        }
+                    
+                        // NOT STATIC
+                        public String helloWorld() {
+                            return helloWorldString;
+                        }
+                    
+                        // NOT STATIC
+                        private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+                            out.write(magicWord.getBytes());
+                        }
+                    
+                        // NOT STATIC
+                        private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+                            in.defaultReadObject();
+                            magicWord = (String) in.readObject();
+                        }
+                    
+                        // NOT STATIC
+                        private void readObjectNoData() throws ObjectStreamException {
+                            magicWord = "Just make something up!";
+                        }
+                    
+                        private static class Toolbox {
+                            private String wrench = "wrench";
+                            private static String drill = "drill";
+                    
+                            // NOT STATIC
+                            public String getWrench() {
+                                return wrench;
+                            }
+                    
+                            // NOT STATIC
+                            public void setWrench(String wrench) {
+                                this.wrench = wrench;
+                            }
+                    
+                            // STATIC
+                            private String getDrill() {
+                                Utilities utilities = new Utilities() {
+                    
+                                    // NOT STATIC (static anonymous class method declarations are not supported at language level 8)
+                                    private void helloWorldFromAnonymousClass() {
+                                        System.out.println("hello world from anonymous class!");
+                                    }
+                                };
+                    
+                    
+                                return Toolbox.drill;
+                            }
+                    
+                            // NOT STATIC
+                            public void setDrill(String drill) {
+                                Toolbox.drill = drill;
+                            }
+                    
+                            // STATIC
+                            private void changeDrill(String drill) {
+                                if (c || d) {
+                                    Toolbox.drill = drill;
+                                }
+                            }
                         }
                     }
-                }
-                
                 """, """
-                import java.io.IOException;
-                import java.io.ObjectStreamException;
-                import java.io.Serializable;
                 
-                public class Utilities implements Serializable {
-                    private static String magicWord = "magic";
-                    private boolean magicWordIsLocked = true;
-                    private static final String helloWorldString = "Hello world!";
+                    import java.io.IOException;
+                    import java.io.ObjectStreamException;
+                    import java.io.Serializable;
                     
-                    public boolean a,b = true;
-                
-                    // STATIC
-                    private static String getMagicWord() {
-                        return magicWord;
-                    }
-                
-                    // NOT STATIC
-                    private void setMagicWord(String newMagicWord) {
-                        if (!magicWordIsLocked) {
-                            magicWord = newMagicWord;
-                        }
-                    }
-                
-                    // NOT STATIC
-                    public boolean getMagicWordIsLocked() {
-                        return magicWordIsLocked;
-                    }
-                
-                    // NOT STATIC
-                    public void setMagicWordIsLocked(boolean magicWordIsLocked) {
-                        this.magicWordIsLocked = magicWordIsLocked;
-                    }
-                
-                    // NOT STATIC
-                    public String helloWorld() {
-                        return helloWorldString;
-                    }
-                
-                    // NOT STATIC
-                    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
-                        out.write(magicWord.getBytes());
-                    }
-                
-                    // NOT STATIC
-                    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
-                        in.defaultReadObject();
-                        magicWord = (String) in.readObject();
-                    }
-                
-                    // NOT STATIC
-                    private void readObjectNoData() throws ObjectStreamException {
-                        magicWord = "Just make something up!";
-                    }
-                
-                    private static class Toolbox {
-                        private String wrench = "wrench";
-                        private static String drill = "drill";
-                
-                        // NOT STATIC
-                        public String getWrench() {
-                            return wrench;
-                        }
-                
-                        // NOT STATIC
-                        public void setWrench(String wrench) {
-                            this.wrench = wrench;
-                        }
-                
+                    public class Utilities implements Serializable {
+                        private static String magicWord = "magic";
+                        private boolean magicWordIsLocked = true;
+                        private static final String helloWorldString = "Hello world!";
+                    
+                        public boolean a, b = true;
+                        public static boolean c, d = true;
+                    
                         // STATIC
-                        private static String getDrill() {
-                            return drill;
+                        private static String getMagicWord() {
+                            return magicWord;
                         }
-                
+                    
                         // NOT STATIC
-                        public void setDrill(String drill) {
-                            Toolbox.drill = drill;
+                        private String getExtraMagicalWord() {
+                            if (a && b) {
+                                return "Extra " + magicWord;
+                            }
+                            return magicWord;
+                        }
+                    
+                        // NOT STATIC
+                        private void setMagicWord(String newMagicWord) {
+                            if (!magicWordIsLocked) {
+                                magicWord = newMagicWord;
+                            }
+                        }
+                    
+                        // NOT STATIC
+                        public boolean getMagicWordIsLocked() {
+                            return magicWordIsLocked;
+                        }
+                    
+                        // NOT STATIC
+                        public void setMagicWordIsLocked(boolean magicWordIsLocked) {
+                            this.magicWordIsLocked = magicWordIsLocked;
+                        }
+                    
+                        // NOT STATIC
+                        public String helloWorld() {
+                            return helloWorldString;
+                        }
+                    
+                        // NOT STATIC
+                        private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+                            out.write(magicWord.getBytes());
+                        }
+                    
+                        // NOT STATIC
+                        private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+                            in.defaultReadObject();
+                            magicWord = (String) in.readObject();
+                        }
+                    
+                        // NOT STATIC
+                        private void readObjectNoData() throws ObjectStreamException {
+                            magicWord = "Just make something up!";
+                        }
+                    
+                        private static class Toolbox {
+                            private String wrench = "wrench";
+                            private static String drill = "drill";
+                    
+                            // NOT STATIC
+                            public String getWrench() {
+                                return wrench;
+                            }
+                    
+                            // NOT STATIC
+                            public void setWrench(String wrench) {
+                                this.wrench = wrench;
+                            }
+                    
+                            // STATIC
+                            private static String getDrill() {
+                                Utilities utilities = new Utilities() {
+                    
+                                    // NOT STATIC (static anonymous class method declarations are not supported at language level 8)
+                                    private void helloWorldFromAnonymousClass() {
+                                        System.out.println("hello world from anonymous class!");
+                                    }
+                                };
+                    
+                    
+                                return Toolbox.drill;
+                            }
+                    
+                            // NOT STATIC
+                            public void setDrill(String drill) {
+                                Toolbox.drill = drill;
+                            }
+                    
+                            // STATIC
+                            private static void changeDrill(String drill) {
+                                if (c || d) {
+                                    Toolbox.drill = drill;
+                                }
+                            }
                         }
                     }
-                }
-
                 """));
     }
 }
